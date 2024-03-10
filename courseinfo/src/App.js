29474524195b64930b385627ef548d3ccd31324c
Daabeat <danielgoal76@gmail.com>
2024-03-10 20:45:11 +0100

    Commit courseinfo + JS intro exercises

---
+++ courseinfo/src/App.js
@@ -0,0 +1,58 @@
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const Total = ({ parts }) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map((parte, index) => (
+        <Part key={index} part={parte} />
+      ))}
+    </>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default App;
--- part1/src/Mensaje.js
+++
@@ -1,7 +0,0 @@
-const Mensaje = (props) => {
-  console.log(props);
-
-  return <h1 style={{ color: props.color }}>{props.message}</h1>;
-};
-
-export default Mensaje;
